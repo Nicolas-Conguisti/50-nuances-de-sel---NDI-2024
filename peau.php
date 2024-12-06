8e209de0931680757430253e6fa2fe079dfcd1c6
Yxshad <elavergne001@iutbayonne.univ-pau.fr>
2024-12-06 02:33:04 +0100

    feat: Ajout du texte éducatif principal

--- peau.php
+++ peau.php
@@ -103,9 +103,25 @@ $anneeActuelle = date('Y');
 
     <main>
         <section>
+        <h2>La peau</h2>
+        <p>La peau a beaucoup d'utilités et celle qui nous intéresse principalement pour notre comparaison est son abilité à gérer notre température.<br>
+        La peau, entre autre, nous évite de complètement fondre de l'intérieur en prenant une douche chaude, après un effort physique, à côté d'un chauffage etc...
+        </p>
+        <h2>La banquise</h2>
+        <p>Maintenant, la banquise : Depuis 1970, on a perdu 13.1% de notre banquise.<br>
+        Beaucoup pensent que la fonte de la banquise fait monter le niveau des océans : Non.<br>
+        EN REVANCHE, la banquise a une albédo très proche de 1 (la qualifiant par conséquent de surface miroitante). Cela fait donc qu'une grande majorité des rayons solaires sont reflétés par la banquise.</br>
+        Ainsi, on se retrouve avec moins de banquise, donc moins de réflexion des rayons pour que les océans, eux, surface non miroitante, les absorbent.<br>
+    Si l'océan est notre corps, alors sans sa peau (=la banquise), il absorbe la chaleur, va se réchauffer, va devenir plus acide et va se détériorer.</p>
+        </section>
+
+        
+        <section>
+            <p>Sur une autre note....</p>
             <h2>Bienvenue sur le jeu de l'Ours Polaire</h2>
             <p>
                 Aidez l'ours polaire à éviter les trous sur la banquise en sautant ! <br>
+                Lui aussi vit très mal la fonte de la banquise !!!<br>
                 Appuyez sur **Espace** pour sauter lorsque le jeu commence.
             </p>
 
